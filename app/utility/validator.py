dfe09ba7c2922714a8f1b43849142244a7ce3eb6
pomelooooooo <suwijak1691@gmail.com>
2020-09-06 13:49:19 +0700

    readme

--- app/utility/validator.py
+++ app/utility/validator.py
@@ -1,4 +1,12 @@
 def validate_name(name):
+    blacklist = ['0', '1', '2', '3', '4', '5',
+                 '6', '7', '8', '9', '!', '@', '#', '$', ' ']
+    if name == "":
+        return False
+
+    for char in name:
+        if char in blacklist:
+            return False
     return True
 
 
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -4,7 +4,22 @@ from app.utility.validator import validate_name, validate_id, validate_phone_num
 
 class TestUtility(unittest.TestCase):
     def test_validate_name_with_valid_input(self):
-        self.assertEqual(True, validate_name("hanif"))
+        self.assertEqual(True, validate_name("king"))
+
+    def test_validate_name_with_valid_input_string_of_int(self):
+        self.assertEqual(False, validate_name("1234"))
+
+    def test_validate_name_with_valid_input_contained_string_of_int(self):
+        self.assertEqual(False, validate_name("king1234"))
+
+    def test_validate_name_with_valid_input_spacial_char(self):
+        self.assertEqual(True, validate_name("F"))
+
+    def test_validate_name_with_valid_input_empty_char(self):
+        self.assertEqual(False, validate_name(""))
+
+    def test_validate_name_with_valid_input_space_char(self):
+        self.assertEqual(False, validate_name("ff fff"))
 
 
 if __name__ == '__main__':
